63162e7cadb552d070ba8a99390aba5c64ad78d4
Seohyun Jung <sxj393@case.edu>
2020-09-11 15:28:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#UTM Simulator
+
+UTM Simulator is designed to simulate small univeral turing machines specified in 
+"<em>Small Universal Turing machines. Theoretical Computer Science. 168 (1996) 215-240</em>" by Yuri Rogozhin.
+
---
+++ src/utm_simulation/gui/SimulationRunner.java
@@ -0,0 +1,15 @@
+package utm_simulation.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimulationRunner extends Application{
+    @Override
+    public void start(Stage stage){
+
+    }
+
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+}
